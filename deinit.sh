1600721acd061e3182a8b49aa9060124caa38d14
vitaminwater <stantin04@gmail.com>
2019-02-06 20:14:53 +0100

    first tests

---
+++ deinit.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -XPOST 'http://192.168.4.1/i?k=BOX_1_ENABLED&v=0'
---
+++ init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -XPOST 'http://192.168.4.1/i?k=BOX_1_ENABLED&v=1'
+curl -XPOST 'http://192.168.4.1/i?k=BOX_1_BLOWER_ENABLED&v=1'
