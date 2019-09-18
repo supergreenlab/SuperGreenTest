dc03cd40a1fd73ed778171a6ff24b6ac9c4802e7
vitaminwater <stantin04@gmail.com>
2019-09-18 15:53:45 +0200

    v2 update

--- test.sh
+++ test.sh
@@ -1,34 +1,43 @@
 #!/bin/bash
 
-# curl -XPOST 'http://supergreendriver.local/i?k=LED_4_BOX&v=0'
-# curl -XPOST 'http://supergreendriver.local/i?k=LED_5_BOX&v=0'
+SERVER="$1"
+
+if [ -z "$SERVER" ]; then
+  echo "USAGE: $0 address"
+  exit
+fi
 
 for i in {0..5}; do
-  curl -XPOST "http://supergreendriver.local/i?k=LED_${i}_DUTY&v=10"
-  sleep 0.1
+  echo "setting led $i to 10%: $(curl -s -XPOST "http://$SERVER/i?k=LED_${i}_DUTY&v=10")"
 done
+
+sleep 2
+echo "Sleep 2s"
 
 for i in {0..5}; do
-  curl -XPOST "http://supergreendriver.local/i?k=LED_${i}_DUTY&v=0"
-  sleep 0.1
+  echo "setting led $i to 0%: $(curl -s -XPOST "http://$SERVER/i?k=LED_${i}_DUTY&v=0")"
 done
 
-for i in {0..1}; do
-  curl -XPOST "http://supergreendriver.local/i?k=BOX_${i}_BLOWER&v=100"
-  sleep 1
+for i in {0..2}; do
+  echo "setting blower to 100%: $(curl -s -XPOST "http://$SERVER/i?k=BOX_${i}_BLOWER_NIGHT&v=100")"
 done
 
-for i in {0..1}; do
-  curl -XPOST "http://supergreendriver.local/i?k=BOX_${i}_BLOWER&v=0"
+echo "Sleep 5s"
+sleep 5
+
+for i in {0..2}; do
+  echo "setting blower to 30%: $(curl -s -XPOST "http://$SERVER/i?k=BOX_${i}_BLOWER_NIGHT&v=30")"
 done
-
-
-# curl -XPOST 'http://supergreendriver.local/i?k=LED_4_BOX&v=1'
-# curl -XPOST 'http://supergreendriver.local/i?k=LED_5_BOX&v=1'
 
 echo
 echo Reading temp/humi sensor values...
-echo temp: $(curl -s http://supergreendriver.local/i?k=BOX_0_SHT21_TEMP_C) humi: $(curl -s http://supergreendriver.local/i?k=BOX_0_SHT21_HUMI)
+echo temp: $(curl -s http://$SERVER/i?k=BOX_0_TEMP) humi: $(curl -s http://$SERVER/i?k=BOX_0_HUMI)
 
-echo Downloading config.yml.gz
-curl  http://supergreendriver.local/fs/config.yml.gz >> /dev/null
+echo Downloading config.json
+ret="$(curl -s http://$SERVER/fs/config.json)
+
+if [ "$ret" == "This URI does not exist" ]; then
+  echo "Failed to load config.json"
+else
+  echo "Config.json loaded"
+fi 
