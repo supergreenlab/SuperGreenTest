d2fa4a84e550adb891b2c60cb8565c823c90eb6a
FreakingDuck <charles@passet.org>
2019-09-25 14:02:53 +0200

    Update

--- test.sh
+++ test.sh
@@ -7,11 +7,13 @@ if [ -z "$SERVER" ]; then
   exit
 fi
 
+curl -XPOST 'http://192.168.4.1/i?k=BOX_2_ENABLED&v=1'
+
 for i in {0..5}; do
   echo "setting led $i to 10%: $(curl -s -XPOST "http://$SERVER/i?k=LED_${i}_DUTY&v=10")"
 done
 
-sleep 2
+sleep 5
 echo "Sleep 2s"
 
 for i in {0..5}; do
@@ -20,24 +22,25 @@ done
 
 for i in {0..2}; do
   echo "setting blower to 100%: $(curl -s -XPOST "http://$SERVER/i?k=BOX_${i}_BLOWER_NIGHT&v=100")"
+  sleep 3
+  echo "setting blower to 0%: $(curl -s -XPOST "http://$SERVER/i?k=BOX_${i}_BLOWER_NIGHT&v=0")"
+  sleep 1
 done
 
-echo "Sleep 5s"
-sleep 5
+for i in {0..2}; do
+  echo "setting blower to 100%: $(curl -s -XPOST "http://$SERVER/i?k=BOX_${i}_BLOWER_NIGHT&v=100")"
+done
+
+echo "Sleep 2s"
+sleep 2
 
 for i in {0..2}; do
-  echo "setting blower to 30%: $(curl -s -XPOST "http://$SERVER/i?k=BOX_${i}_BLOWER_NIGHT&v=30")"
+  echo "setting blower to 100%: $(curl -s -XPOST "http://$SERVER/i?k=BOX_${i}_BLOWER_NIGHT&v=0")"
 done
 
 echo
 echo Reading temp/humi sensor values...
 echo temp: $(curl -s http://$SERVER/i?k=BOX_0_TEMP) humi: $(curl -s http://$SERVER/i?k=BOX_0_HUMI)
+echo temp: $(curl -s http://$SERVER/i?k=BOX_1_TEMP) humi: $(curl -s http://$SERVER/i?k=BOX_1_HUMI)
 
-echo Downloading config.json
-ret="$(curl -s http://$SERVER/fs/config.json)
-
-if [ "$ret" == "This URI does not exist" ]; then
-  echo "Failed to load config.json"
-else
-  echo "Config.json loaded"
-fi 
+curl -XPOST 'http://192.168.4.1/i?k=BOX_2_ENABLED&v=0'
