f39cd41647a22b04a25157f3a623ac805d4ecd78
vitaminwater <stantin04@gmail.com>
2020-04-17 16:53:50 +0200

    updated tests

--- deinit.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -XPOST 'http://192.168.4.1/i?k=BOX_1_ENABLED&v=0'
--- test.sh
+++ test.sh
@@ -7,8 +7,6 @@ if [ -z "$SERVER" ]; then
   exit
 fi
 
-curl -XPOST 'http://192.168.4.1/i?k=BOX_2_ENABLED&v=1'
-
 for i in {0..5}; do
   echo "setting led $i to 10%: $(curl -s -XPOST "http://$SERVER/i?k=LED_${i}_DUTY&v=10")"
 done
@@ -21,26 +19,12 @@ for i in {0..5}; do
 done
 
 for i in {0..2}; do
-  echo "setting blower to 100%: $(curl -s -XPOST "http://$SERVER/i?k=BOX_${i}_BLOWER_NIGHT&v=100")"
-  sleep 3
-  echo "setting blower to 0%: $(curl -s -XPOST "http://$SERVER/i?k=BOX_${i}_BLOWER_NIGHT&v=0")"
-  sleep 1
-done
-
-for i in {0..2}; do
-  echo "setting blower to 100%: $(curl -s -XPOST "http://$SERVER/i?k=BOX_${i}_BLOWER_NIGHT&v=100")"
-done
-
-echo "Sleep 2s"
-sleep 2
-
-for i in {0..2}; do
-  echo "setting blower to 100%: $(curl -s -XPOST "http://$SERVER/i?k=BOX_${i}_BLOWER_NIGHT&v=0")"
+  echo "setting blower to 100%: $(curl -s -XPOST "http://$SERVER/i?k=BOX_${i}_BLOWER_DUTY&v=100")"
+  sleep 2
+  echo "setting blower to 0%: $(curl -s -XPOST "http://$SERVER/i?k=BOX_${i}_BLOWER_DUTY&v=0")"
 done
 
 echo
 echo Reading temp/humi sensor values...
 echo temp: $(curl -s http://$SERVER/i?k=BOX_0_TEMP) humi: $(curl -s http://$SERVER/i?k=BOX_0_HUMI)
 echo temp: $(curl -s http://$SERVER/i?k=BOX_1_TEMP) humi: $(curl -s http://$SERVER/i?k=BOX_1_HUMI)
-
-curl -XPOST 'http://192.168.4.1/i?k=BOX_2_ENABLED&v=0'
